cfb47066e7cd76bce17375b9cad69a2ff7a490d0
Ryan Andersen <ryco117@gmail.com>
2021-05-30 14:15:28 -0700

    Updated licensing information in source, fixed simple TODO

--- FunctionalSoul.Android/AssemblyInfo.fs
+++ FunctionalSoul.Android/AssemblyInfo.fs
@@ -1,7 +1,23 @@
-﻿namespace FunctionalSoul.Android
+﻿(*
+This file is part of FunctionalSoul
+
+FunctionalSoul is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FunctionalSoul is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FunctionalSoul. If not, see <https://www.gnu.org/licenses/>.
+*)
+
+namespace FunctionalSoul.Android
 
 open System.Reflection
-open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 open Android.App
 
@@ -32,10 +48,10 @@ type Resources = FunctionalSoul.Android.Resource
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("0.0.0.2")>]
+[<assembly: AssemblyFileVersion("0.0.0.2")>]
 
 // Add some common permissions, these can be removed if not needed
 [<assembly: UsesPermission(Android.Manifest.Permission.Internet)>]
 [<assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)>]
-do()
+do ()
--- FunctionalSoul/FileSearchPage.fs
+++ FunctionalSoul/FileSearchPage.fs
@@ -1,4 +1,21 @@
-﻿module FileSearchPage
+﻿(*
+This file is part of FunctionalSoul
+
+FunctionalSoul is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FunctionalSoul is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FunctionalSoul. If not, see <https://www.gnu.org/licenses/>.
+*)
+
+module FileSearchPage
 
 open Fabulous
 open Fabulous.XamarinForms
@@ -50,7 +67,13 @@ let update environment msg model =
         | CompletedSearchPattern pattern -> FileSearch {model with searchPattern = pattern}, Cmd.none
         | SearchPatternStart dispatch -> FileSearch {model with userFileMatches = []; searching = true}, searchPattern model dispatch
         | SearchPatternUserFiles files -> FileSearch {model with userFileMatches = files::model.userFileMatches}, Cmd.none
-        | SearchPatternFinish _result -> FileSearch {model with searching = false}, Cmd.none    // TODO: Handle FileSearch errors
+        | SearchPatternFinish result ->
+            let newModel = FileSearch {model with searching = false}
+            let cmd =
+                match result with
+                | Ok () -> Cmd.none
+                | Error message -> Helpers.createSnackbarInfo 5000. message |> CreateSnackbar |> Cmd.ofMsg
+            newModel, cmd
         | SelectedFileIndexOption (Some i) ->
             let user, file = Helpers.userFileByIndex model.userFileMatches i
             FileSearch {model with download = Some {client = model.client; progress = None; peer = user; selectedFile = file}}, Cmd.none
